2b50996ffd3f611f3d09acabc2c2e6ceb0cc99f4
Dave Roberge <david.roberge@maine.edu>
2012-08-03 09:30:36 -0400

    Fixed generics on FormProvider

--- motech-mobileforms-api/src/main/java/org/motechproject/mobileforms/api/service/FormProvider.java
+++ motech-mobileforms-api/src/main/java/org/motechproject/mobileforms/api/service/FormProvider.java
@@ -3,7 +3,7 @@ package org.motechproject.mobileforms.api.service;
 import org.motechproject.mobileforms.api.domain.FormBean;
 import org.motechproject.mobileforms.api.validator.FormValidator;
 
-public interface FormProvider<T extends FormBean, V extends FormValidator<? extends FormBean>> {
+public interface FormProvider<T extends FormBean, V extends FormValidator<T>> {
 
     T makeInstance();
 
