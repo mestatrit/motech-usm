7b7cb4ff8875028c3bf94cd71b7ae953898debc7
vishnu karthik <karthikl@thoughtworks.com>
2013-05-09 11:49:07 +0530

    Vishnu | API to get all the mappings

--- src/main/java/org/motechproject/mapper/web/controllers/MappingFileController.java
+++ src/main/java/org/motechproject/mapper/web/controllers/MappingFileController.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -39,5 +36,11 @@ public class MappingFileController {
         List<MRSMapping> mrsMappings = mappingsReader.readJson(mappingJson);
         mrsMappingService.addOrUpdate(mrsMappings);
         return "redirect:" + httpServletRequest.getHeader("Referer");
+    }
+
+    @RequestMapping(value = "/getAllMappings", method = RequestMethod.GET)
+    @ResponseBody
+    public List<MRSMapping> getAllMappings() {
+        return mrsMappingService.getAllMappings();
     }
 }
--- src/main/resources/webapp/js/controllers.js
+++ src/main/resources/webapp/js/controllers.js
@@ -1,5 +1,17 @@
 /* put your angular controllers here */
 
-function uploadController($scope) {
+function uploadController($scope, $http) {
+    $scope.method = 'GET';
+    $scope.url = '../commcare-mrs-mapper/getAllMappings';
 
+    $http({method: $scope.method, url: $scope.url}).
+        success(function (data, status) {
+            $scope.status = status;
+            $scope.mappings = data;
+
+        }).
+        error(function (data, status) {
+            $scope.data = data || "Request failed";
+            $scope.status = status;
+        });
 }
